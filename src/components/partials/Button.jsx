f88d603efc01d26e6eecf3f291ac33a743c4ce71
Rajesh8531 <rajesh99ed@gmail.com>
2025-01-25 22:45:54 +0530

    image path updated

--- src/components/partials/Button.jsx
+++ src/components/partials/Button.jsx
@@ -16,7 +16,7 @@ function Button() {
       className="bg-[#04a7ee] text-white md:px-3  lg:px-6 py-3 px-10 rounded-sm md:ml-3 lg:ml-8 hover:bg-[#0c3a85] duration-150 flex items-center"
     >
       <img
-        src="/images/download.png"
+        src="/assignment-contact/images/download.png"
         alt="Download Icon"
         className="w-6 h-6 mr-2 md:w-4 md:h-4"
       />
